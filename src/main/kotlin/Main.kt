69d5fc0080792cd9aa454b1c9617f0e7d47b580f
Kevin David Molina Gómez <kevin.molina.go@gmail.com>
2021-10-21 11:19:33 -0500

    agregue recursividad.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,31 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments at Run/Debug configuration
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    // sumadorRecursivo(0, 100)
+    //println(suma(10, 20))
+    println(factorial(10f))
 }
+
+fun factorial(numero: Float): Float {
+    if (numero > 1){
+        return numero * factorial(numero - 1)
+    } else {
+        return numero
+    }
+}
+
+fun suma(numero1: Int, numero2: Int): Int {
+    return numero1 + numero2
+}
+
+fun sumadorRecursivo(i: Int, n: Int) {
+
+    var contador = i
+    println("INICIO DEL SUMADOR")
+
+    if (contador < n) {
+        contador++
+        sumadorRecursivo(contador, n)
+    }
+
+}
+
+
